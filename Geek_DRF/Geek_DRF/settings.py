382671dc61696ecae5d1dc2ae52bd6e2ae0618f8
Romnako <76477868+Romnako@users.noreply.github.com>
2022-02-28 20:22:39 +0300

    HW Lesson_2-DRF

--- Geek_DRF/Geek_DRF/settings.py
+++ Geek_DRF/Geek_DRF/settings.py
@@ -39,11 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authors',
     'rest_framework',
+    'corsheaders',
 ]
+
+# Auth model
+AUTH_USER_MODEL = "authors.Author"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +130,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
--- Geek_DRF/authors/models.py
+++ Geek_DRF/authors/models.py
@@ -1,10 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 from uuid import uuid4
 
-from django.db import models
+from django.db.models.fields import UUIDField
 
 
-class Author(models.Model):
-    uid = models.UUIDField(primary_key=True, default=uuid4)
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-    birthday_year = models.PositiveIntegerField()
+class Author(AbstractUser):
+    uuid = UUIDField(primary_key=True, default=uuid4)
+    email = models.EmailField(unique=True)
+
+# from uuid import uuid4
+#
+# from django.db import models
+#
+#
+# class Author(models.Model):
+#     uid = models.UUIDField(primary_key=True, default=uuid4)
+#     first_name = models.CharField(max_length=64)
+#     last_name = models.CharField(max_length=64)
+#     birthday_year = models.PositiveIntegerField()
