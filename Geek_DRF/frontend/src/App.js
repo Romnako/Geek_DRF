274def6e7ba810e459e488c59e31d329e41e62b7
Romnako <76477868+Romnako@users.noreply.github.com>
2022-03-31 16:12:20 +0300

    HW Lesson_7_DRF

--- Geek_DRF/frontend/src/App.js
+++ Geek_DRF/frontend/src/App.js
@@ -8,6 +8,9 @@ import { BrowserRouter as Router, Route, Routes< Navigate, Link, Switch } from '
 import ToDoList from './components/ToDo';
 import ProjectList from '/components/Project';
 import BookList from '/components/Book';
+import Cookies from 'universal-cookie';
+import LoginForm from './components/Auth';
+import { Link } from 'react-router-dom';
 
 
 const NotFound404 = ({ location }) => {
@@ -26,10 +29,76 @@ class App extends React.Component {
       'authors': [],
       'books' : [],
       'todos': [],
-      'projects': []
+      'projects': [],
+      'token': ''
     }
   }
 
+ set_token(token) {
+    const cookies = new Cookies()
+    cookies.set('token', token)
+    this.setState({'token': token}, ()=>this.load_data())
+  }
+
+  is_authenticated() {
+    return this.state.token !== ''
+  }
+
+  logout() {
+    this.set_token('')
+  }
+
+  get_token_from_storage() {
+    const cookies = new Cookies()
+    const token = cookies.get('token')
+    this.setState({'token': token}, ()=>this.load_data())
+  }
+
+  get_token(username, password) {
+    axios.post('http://127.0.0.1:8000/api-token-auth/', {username: username, password: password})
+    .then(response => {
+        this.set_token(response.data['token'])
+    }).catch(error => alert('Неверный логин или пароль'))
+  }
+
+  get_headers() {
+    let headers = {
+      'Content-Type': 'application/json'
+    }
+  if (this.is_authenticated())
+    {
+        headers['Authorization'] = 'Token ' + this.state.token
+    }
+    return headers
+  }
+
+
+  load_data() {
+
+    const headers = this.get_headers()
+    axios.get('http://127.0.0.1:8000/api/authors/', {headers})
+        .then(response => {
+            const users = response.data.results
+            this.setState({users: users})
+        }).catch(error => console.log(error))
+
+    axios.get('http://127.0.0.1:8000/api/projects/', {headers})
+        .then(response => {
+            const projects = response.data.results
+            this.setState({projects: projects})
+        }).catch(error => {
+          console.log(error)
+          this.setState({projects: []})
+        })
+    axios.get('http://127.0.0.1:8000/api/todos/', {headers})
+        .then(response => {
+            const todos = response.data.results
+            this.setState({todos: todos})
+        }).catch(error => {
+          console.log(error)
+          this.setState({todos: []})
+        })
+  }
 
   componentDidMount() {
     axios.get('http://127.0.0.1:8000/api/authors')
@@ -73,6 +142,7 @@ class App extends React.Component {
                   <Route path='/todos' element={<ToDoList todos={this.state.todos} />} />
                   <Route path='/projects' element={<ProjectList projects={this.state.projects} />} />
                   <Route path='/' element={<Navigate replace to="/authors" />} />
+                  <Route path='/login' element={<LoginForm get_token={(username, password) => this.get_token(username, password)} />} />
                   <Route component={NotFound404} />
                 </Routes>
             </Switch>
